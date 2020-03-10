f44dba63b367403557c274dda63505018865447a
Kevin <kevin.carvajal.malmo@gmail.com>
2020-03-10 13:42:28 +0100

    Changed input
    looks changed, put a prevendefault aswell

--- my-app/src/views/RegisterPage/index.js
+++ my-app/src/views/RegisterPage/index.js
@@ -3,22 +3,32 @@ import { Container, Form, Col, Row, Input, Label, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
+    function HandleClick(e){
+        e.preventDefault();
+        console.log('was clicked');
+    }
+
   return (
     <Container fluid className="RegisterPage">
       <h2 className="page-title">Registera</h2>
 
      <Form className="RegisterForm">
     <Row>
-        <Col sm={12} className="name">
+        <Col className="name">
+        <Label>Efternamn</Label>
+        <Input placeholder="Efternamn" type="text"></Input>
+        </Col>
+        <Col className="name">
         <Label>Förnamn</Label>
         <Input placeholder="Förnamn" type="text"></Input>
         </Col>
+        
     </Row>
 
     <Row>
-    <Col sm={12} className="name">
-        <Label>Efternamn</Label>
-        <Input placeholder="Efternamn" type="text"></Input>
+    <Col className="name">
+        <Label>Användarnamn</Label>
+        <Input placeholder="bengt12" type="text"></Input>
         </Col>
     </Row>
     <Row>
@@ -47,9 +57,12 @@ const RegisterPage = () => {
         <Input placeholder="********" type="password"></Input>
         </Col>
     </Row>
+    <Button
+    className="primary-btn sending"
+    onClick={HandleClick}
+    >Skapa konto</Button>
      </Form>
 
-     <Button className="primary-btn">Skapa konto</Button>
      
      <Row className="info">
          <Col>Har du redan ett konto?</Col>
